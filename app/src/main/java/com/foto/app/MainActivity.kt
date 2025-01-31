8984ab1d118bad6770a57e800ea8433ef2188e42
Andrew Kozak <Andrzejimenynnyk@gmail.com>
2025-01-31 13:59:36 +0100

    cal

--- app/src/main/java/com/foto/app/MainActivity.kt
+++ app/src/main/java/com/foto/app/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.foto.app.feature.calculator.CalculatorScreen
 import com.foto.app.feature.photo.FotoScreen
 import com.foto.app.ui.theme.FotoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
             FotoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column (modifier = Modifier.padding(innerPadding))  {
-                        FotoScreen()
+                        CalculatorScreen()
                     }
 
 
